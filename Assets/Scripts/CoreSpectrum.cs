bb00f5ce1d6147921cf0ca8125d72d8fd2da2824
Docter60 <brianboellner@gmail.com>
2018-02-21 02:38:30 -0500

    Audio loopback implementation
    CSCore's WASAPI Loopback Capture is being not playing nice...data is halted for no reason when audio is clearly still playing.  Look into different solutions.

---
+++ Assets/Scripts/CoreSpectrum.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CSCore.SoundIn;
+using CSCore.DSP;
+using CSCore;
+using System;
+using CSCore.SoundOut;
+
+public class CoreSpectrum {
+    private static readonly int CAPTURE_DELAY = 50;
+    private static readonly int STEREO = 2;
+
+    private static float[] hamming;
+
+    private WasapiLoopbackCapture capture;
+    private FftProvider fftProvider; // Still use fft providers? This may be the bug for the fft "pausing"
+    private SilenceGenerator silence;
+    private float[] spectrumData;
+    private bool newDataRead;
+
+    public CoreSpectrum(int size=1024)
+    {
+        newDataRead = true;
+
+        hamming = new float[size];
+        for(int n = 0; n < hamming.Length; n++)
+        {
+            hamming[n] = FastFourierTransformation.HammingWindowF(n, size);
+        }
+
+        capture = new WasapiLoopbackCapture(CAPTURE_DELAY);
+        capture.DataAvailable += (s, a) =>
+        {
+            if (newDataRead)
+            {
+                UpdateSpectrumData(a.Data);
+            }
+        };
+        capture.Stopped += (c, e) =>
+        {
+            for(int i = 0; i < spectrumData.Length; i++)
+            {
+                spectrumData[i] = 0f;
+            }
+        };
+        capture.Initialize();
+        capture.Start();
+
+        fftProvider = new FftProvider(STEREO, FftSize.Fft1024);
+
+        spectrumData = new float[size];
+    }
+
+    public float[] GetSpectrumData()
+    {
+        return spectrumData;
+    }
+
+    public void DataRead()
+    {
+        newDataRead = true;
+    }
+
+    private void UpdateSpectrumData(byte[] data)
+    {
+        for (int i = 0; i < (int)fftProvider.FftSize; i++)
+        {
+            float l = BitConverter.ToSingle(data, i * 8) * hamming[i];
+            float r = BitConverter.ToSingle(data, i * 8 + 4) * hamming[i];
+            fftProvider.Add(l, r);
+        }
+
+        lock (spectrumData)
+        {
+            fftProvider.GetFftData(spectrumData);
+        }
+
+        newDataRead = false;
+    }
+
+}
+
+public class SilenceGenerator : IWaveSource
+{
+    private readonly WaveFormat _waveFormat = new WaveFormat(44100, 16, 2);
+
+    public int Read(byte[] buffer, int offset, int count)
+    {
+        Array.Clear(buffer, offset, count);
+        return count;
+    }
+
+    public WaveFormat WaveFormat
+    {
+        get { return _waveFormat; }
+    }
+
+    public long Position
+    {
+        get { return -1; }
+        set
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    public long Length
+    {
+        get { return -1; }
+    }
+
+    public bool CanSeek
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public void Dispose()
+    {
+        //do nothing
+    }
+}
--- Assets/Scripts/Spectrum.cs
+++ Assets/Scripts/Spectrum.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spectrum : MonoBehaviour {
-    private static readonly int DEFAULT_COUNT = 100; 
+    private static int DEFAULT_COUNT = 100;
+    private static float DEFAULT_LERP_DELTA = 7f;
     private static GameObject boxElement;
 
     private List<GameObject> elements;
+
+    private CoreSpectrum coreSpectrum;
 
 	// Use this for initialization
 	void Start () {
@@ -16,14 +19,32 @@ public class Spectrum : MonoBehaviour {
         for(int i = 0; i < DEFAULT_COUNT; i++)
         {
             GameObject element = Instantiate(boxElement);
+            element.transform.parent = gameObject.transform;
             elements.Add(element);
+
+            //temp
+            element.transform.localPosition = new Vector3(i, 0, 0);
         }
+
+        coreSpectrum = new CoreSpectrum();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float[] spectrumData = coreSpectrum.GetSpectrumData();
+
+        for(int i = 0; i < elements.Count; i++)
+        {
+            GameObject element = elements[i];
+            Vector3 newVal = new Vector3(1f, spectrumData[i] * 8000f, 1f);
+            Vector3 oldVal = element.transform.localScale;
+
+            element.transform.localScale = Vector3.Lerp(oldVal, newVal, Time.deltaTime * DEFAULT_LERP_DELTA);
+        }
+        
+        coreSpectrum.DataRead();
+
 	}
 
     // All of these elements need to be repositioned and handled automatically. Add listeners?
